52d02b0080c0858d01f27ef647159f5ec1771b1f
dmitry <pisarev_dmitry@mail.ru>
2021-09-21 19:57:36 +0300

    test

--- src/reducers/channel.js
+++ src/reducers/channel.js
@@ -24,19 +24,17 @@ const channelSlice = createSlice({
     ));
     buider.addCase(addChannel, (state, { payload }) => {
       const newState = { ...state };
-      newState.channels.push(payload.channel);
+      newState.channels = [...state.channels, payload.channel];
       newState.currentChannelId = payload.channel.id;
       return newState;
     });
     buider.addCase(renameChannel, (state, { payload }) => {
-      const newState = { ...state };
-      const renamedChannel = newState.channels.find(({ id }) => id === payload.channel.id);
+      const renamedChannel = state.channels.find(({ id }) => id === payload.channel.id);
       renamedChannel.name = payload.channel.name;
-      return newState;
     });
     buider.addCase(deleteChannel, (state, { payload }) => {
       const newState = { ...state };
-      newState.channels = newState.channels.filter((channel) => channel.id !== payload.id);
+      newState.channels = state.channels.filter((channel) => channel.id !== payload.id);
       const defaultChannel = newState.channels.find(({ name }) => name === 'general');
       newState.currentChannelId = defaultChannel.id;
       return newState;
