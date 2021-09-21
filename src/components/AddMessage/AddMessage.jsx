7148284e2f26729e21d8640868bc277b41eba542
dmitry <pisarev_dmitry@mail.ru>
2021-09-21 16:18:15 +0300

    test

--- src/components/AddMessage/AddMessage.jsx
+++ src/components/AddMessage/AddMessage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import * as yup from 'yup';
 import { Button, InputGroup } from 'react-bootstrap';
+import { getModalStatus } from '../../selectors';
 
 const AddMessage = ({ onSubmit }) => {
   const { t } = useTranslation();
+  const { isOpened } = useSelector(getModalStatus);
   return (
     <Formik
       initialValues={{ message: '' }}
@@ -27,7 +30,7 @@ const AddMessage = ({ onSubmit }) => {
                   d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
                 />
               </svg>
-              <span className="visually-hidden">{t('buttons.send')}</span>
+              {isOpened && <span className="visually-hidden">{t('buttons.send')}</span>}
             </Button>
           </InputGroup>
         </Form>
