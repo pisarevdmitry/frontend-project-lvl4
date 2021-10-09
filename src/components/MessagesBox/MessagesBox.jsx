dd2badb44aacc4ed1a422af51b7507ffa559e03f
dmitry <pisarev_dmitry@mail.ru>
2021-10-09 20:05:07 +0300

    addScrolling

--- src/components/MessagesBox/MessagesBox.jsx
+++ src/components/MessagesBox/MessagesBox.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
-const MessagesBox = ({ messages }) => (
-  <div className="chat-messages overflow-auto px-5">
-    {messages.map(({ userName, body, id }) => (
-      <div key={id} className="text-break mb-2">
-        <b>{userName}</b>
-        {': '}
-        {body}
-      </div>
-    ))}
-  </div>
-);
+const MessagesBox = ({ messages }) => {
+  const container = useRef(null);
+  useEffect(() => {
+    container.current.scrollTop = container.current.scrollHeight;
+  }, [messages]);
+  return (
+    <div ref={container} className="chat-messages overflow-auto px-5">
+      {messages.map(({ userName, body, id }) => (
+        <div key={id} className="text-break mb-2">
+          <b>{userName}</b>
+          {': '}
+          {body}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default MessagesBox;
