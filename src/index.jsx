478d5d8152dc951769dcf2299eb0ead13816e386
dmitry <pisarev_dmitry@mail.ru>
2021-11-05 22:19:35 +0300

    add i18nProvider

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import '../assets/application.scss';
 import { render } from 'react-dom';
 import init from './init.jsx';
 
-const vdom = init();
-
-render(
-  vdom,
-  document.getElementById('chat'),
-);
+init().then((vdom) => {
+  render(
+    vdom,
+    document.getElementById('chat'),
+  );
+});
--- src/init.jsx
+++ src/init.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { io } from 'socket.io-client';
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import { initReactI18next, I18nextProvider } from 'react-i18next';
 import { ru, en } from './locales/index.js';
 import store from './slices';
 import App from './App.jsx';
@@ -13,26 +13,26 @@ import SocketProvider from './SocketProvider.jsx';
 
 const init = (socketClient = io()) => {
   const i18Instance = i18n.createInstance();
-  i18Instance
+  return i18Instance
     .use(initReactI18next)
     .init({
       resources: { ru, en },
       lng: 'ru',
       fallbackLng: 'en',
-
       interpolation: {
         escapeValue: false,
       },
-    });
-  return (
-    <Provider store={store}>
-      <SocketProvider socketClient={socketClient}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </SocketProvider>
-    </Provider>
-  );
+    }).then(() => (
+      <Provider store={store}>
+        <I18nextProvider i18n={i18Instance}>
+          <SocketProvider socketClient={socketClient}>
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          </SocketProvider>
+        </I18nextProvider>
+      </Provider>
+    ));
 };
 
 export default init;
