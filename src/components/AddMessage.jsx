f6b63fb545b80c90e16466cf0e16bc1fbd55117b
dmitry <pisarev_dmitry@mail.ru>
2021-11-22 15:24:17 +0300

    add profanity

--- src/components/AddMessage.jsx
+++ src/components/AddMessage.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
+import filter from 'leo-profanity';
 import * as yup from 'yup';
 import { Button, InputGroup } from 'react-bootstrap';
 
@@ -17,7 +18,7 @@ const AddMessage = ({ onSubmit, currentChannel, isProccessed }) => {
       resetForm();
       inputEl.current.focus();
     };
-    onSubmit(values, onSuccess);
+    onSubmit({ message: filter.clean(values.message) }, onSuccess);
   };
   useEffect(() => {
     inputEl.current.focus();
